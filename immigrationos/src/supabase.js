ad35cfb7b6c3334f34a63d408f0622d2eb0a2790
Immigrationos <carolineazevedolawfirm@gmail.com>
2026-04-27 15:06:52 -0400

    Update supabase.js

--- immigrationos/src/supabase.js
+++ immigrationos/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://madxojolvciphwmeprxg.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_D0GuZfoRefW78H6Pxrk1Mg_zoSLsnUV';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hZHhvam9sdmNpcGh3bWVwcnhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyODAxODgsImV4cCI6MjA5Mjg1NjE4OH0.1Cp254t5LKevojQGZYzSUBucxzTuJSmdOwS1xW1vqJU';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
